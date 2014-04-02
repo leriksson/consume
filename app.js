24a8eaef6e2f140d629d49340f28a3b1581eb15c
Luc Olson <lusydo@gmail.com>
2014-04-02 07:48:24 -0400

    app.js and usage of jquery ui

--- app.js
+++ app.js
@@ -1,4 +1,54 @@
 $(function(){
 
+	var shoppingArray = [];
+
+	function makeList() {
+		$('#shop-list').empty();
+		for (var i = 0; i < shoppingArray.length; i++) {
+			$('#shop-list').append('<li>'+(i + 1)+'. '+shoppingArray[i]+'</li>');
+			// console.log(shoppingArray);
+			}
+		}
+
+
+	$("input").keydown(function(event) {
+		
+		var addList = $(this).val();
+		if (event.which == 13) {
+			event.preventDefault();
+			shoppingArray.push(addList);
+		//	$('#shop-list').append('<li>'+addList+'</li>');
+			makeList();
+			$('input').val("");
+		}
+	});
+
+	$('#clr').click(function(){
+		shoppingArray.length = 0;
+		makeList();
+	});
+
+	$('#priority').click(function(){
+		shoppingArray.sort();
+		makeList();
+	});
+
+	$( "#shop-list" ).sortable({   
+			placeholder: "ui-sortable-placeholder"   
+		});  
+
+// $("input").on("keydown",function search(e) {
+//     if(e.keyCode == 13) {
+//         alert($(this).val());
+//     }
+// });
+
+// $("input").keydown(function(event) {
+//     if (event.which == 13) {
+//         event.preventDefault();
+//         $("form").submit();
+//         $(this).val()
+//     }
+// });
 
 });
